c6a54f175432845f259e4636dd25f59e345f2190
caramelxzc <rizkymadyachandraramadhan@gmail.com>
2025-11-04 12:16:04 +0700

    Add Feature Show Student by ID

--- resources/views/admin/students/index.blade.php
+++ resources/views/admin/students/index.blade.php
@@ -24,15 +24,16 @@
         <td>{{ $student->id }}</td>
         <td>{{ $student->nis }}</td>
         <td>{{ $student->nama_lengkap }}</td>
-        <td>{{ $student->jenis_kelamin }}</td>
+        <td>{{ $student->jenis_kelamin == 'L' ? 'Laki-laki' : 'Perempuan' }}</td>
         <td>{{ $student->nisn }}</td>
         <td>
+          <a href="{{ route('admin.students.show', $student->id) }}" class="btn btn-info btn-sm">Lihat</a>
           <a href="{{ route('admin.students.edit', $student->id) }}" class="btn btn-warning btn-sm">Edit</a>
           <form action="{{ route('admin.students.destroy', $student->id) }}" method="POST" class="d-inline"
             onsubmit="return confirm('Apakah kamu yakin ingin menghapus data siswa ini?')">
             @csrf
             @method('DELETE')
-            <button class="btn btn-danger btn-sm">Hapus</button>
+            <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
           </form>
         </td>
       </tr>
